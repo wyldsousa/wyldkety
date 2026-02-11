afe051108c0f5f4f3b5ea27b5e95fbf3bf70ce00
wyldsousa <wyldsousa321@gmail.com>
2026-02-11 10:49:33 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyBshFJCgKDPLy2dG6GbuvTvP9lV7ehdJjI",
   authDomain: "financenuvem.firebaseapp.com",
   projectId: "financenuvem",
   storageBucket: "financenuvem.firebasestorage.app",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
