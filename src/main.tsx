7d7cacbaf8734345d3f0c5b76e750a862387bf2a
wyldsousa <wyldsousa321@gmail.com>
2026-02-12 11:38:17 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { AuthProvider } from "@/hooks/useAuth";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
